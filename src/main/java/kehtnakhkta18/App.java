bb84924b0d367630a328dc9b4cdeabc7111de8c7
matisroo <56818226+matisroo@users.noreply.github.com>
2020-02-05 09:50:51 +0200

    Add files via upload

--- src/main/java/kehtnakhkta18/App.java
+++ src/main/java/kehtnakhkta18/App.java
@@ -1,63 +1,70 @@
 package kehtnakhkta18;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
-/**
- * Hello world!
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+
 public final class App {
     private App() {
     }
 
     /**
-     * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        try (PrintWriter writer = new PrintWriter(new File("score.csv"))) {
-            Scanner myObj = new Scanner(System.in);
-            System.out.println("Enter username");
 
-            String userName = myObj.nextLine();
-            String turns = null;
+        String username = null; //Ajutine osa, kuni saaks mingid moodi ühendada teiste osadega
+        String skoor = null;
+        String time = null;
+        String turns = null;
 
-            Instant start = Instant.now();
-            //Timeunit.seconds.sleep(5);
-            Instant end = Instant.now();
-            Duration interval = Duration.between(start, end);
+        try {
+            FileReader reader = new FileReader("score.csv");
+            BufferedReader bufferedReader = new BufferedReader(reader);
 
+            String line;
 
-            //midagi midagi, nupu vajutamine
-            //midagi midagi, nupu vajutamine
-            //midagi midagi, nupu vajutamine
-            //midagi midagi, nupu vajutamine
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line); //Et näha mis on score.csv failis, ilma et peaks seda lahti teha
+            }
+            reader.close();
 
+            // Üleval, BufferedReader, all BufferedWriter
 
-            StringBuilder sb = new StringBuilder();
-            sb.append("Name: " + userName);
-            sb.append(',');
-            sb.append("Turns: " + turns);
-            sb.append(',');
-            sb.append("Time: " + interval.getSeconds());
-            sb.append('\n');
+            FileOutputStream outputStream = new FileOutputStream("score.csv", true);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-16");
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
 
-            writer.write(sb.toString());
+            bufferedWriter.write("--------------------");
+            bufferedWriter.newLine();
+            //bufferedWriter.write("Nimi: " + username);
+            bufferedWriter.write("Nimi: " + "Mirgus"); //Ajutine
+            bufferedWriter.newLine();
+            //bufferedWriter.write("Käike: " + turns);
+            bufferedWriter.write("Käike: " + "23"); //Ajutine
+            bufferedWriter.newLine();
+            //bufferedWriter.write("Aeg: " + time);
+            bufferedWriter.write("Aeg: " + "00:02:20"); //Ajutine
+            bufferedWriter.newLine();
+            //bufferedWriter.write("Skoor: " + skoor);
+            bufferedWriter.write("Skoor: " + "694201337"); //Ajutine
+            bufferedWriter.newLine();
+            bufferedWriter.write("---------------------");
+            bufferedWriter.newLine();
 
-
-
-
-
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
 }
+
+
 
 
 
